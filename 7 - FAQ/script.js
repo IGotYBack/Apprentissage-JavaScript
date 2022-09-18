d0befa383b0a3461400cc535ac363d90c43eacc6
IGotYB <igotyb.stream@gmail.com>
2022-09-18 18:29:49 +0200

    Ajout de JS et petites modifications HTML et CSS

--- 7 - FAQ/script.js
+++ 7 - FAQ/script.js
@@ -1,1 +1,36 @@
 console.log('Salut les canards')
+
+// sélectionne et stocke
+
+const questions = document.querySelectorAll(".question");
+console.log(questions)
+
+// fonctions
+
+// soumet à l'action 
+
+/* 
+   au clic sur chaque élément du tableau, 
+    la réponse correspondante apparaît
+    l'icône change
+   au deuxième clic sur la question
+    la réponse correspondante disparaît
+    l'icône revient à son état normal 
+*/
+
+questions.forEach((item) => {
+    console.log(item, "item")
+    item.addEventListener("click", () => {
+        // console.log("cékliklé")
+        // je sélectionne et stocke la div suivante de l'item cliqué
+        const next = item.nextElementSibling;
+        const icone = item.lastElementChild;
+        // console.log(next)
+        next.classList.toggle("hidden")
+        next.classList.toggle("visible")
+
+        icone.classList.toggle("fa-chevron-down")
+        icone.classList.toggle("fa-chevron-up")
+
+    })
+})
